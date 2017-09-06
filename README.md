912320e572f1030912ac2b686caa90a61bacc936
chousheng <vsked@163.com>
2017-09-06 15:27:45 +0800

    spring mvc+rbac+mysql+shiro

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rbacmysql
+
+this is easyui + springmvc + mybatis + shiro project
+
+mysql database
+
+implements RBAC model and add menu
+
+use easyui front
+
+
+http://localhost:8080/rbacmysql
+
+
+
+当有多种操作在同一个service方法时手工回滚事务(暂未研究出自动化解决方案)
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
---
+++ documents/sqlscript/initTableSpaceAndUse.sql
@@ -0,0 +1,16 @@
+﻿
+/**
+ *this code by vsked
+ *create 201709050947
+ *lastModify 201709050947
+ *任何人有字段修改需要在注释与本头部进行声明！
+*/
+
+
+CREATE USER 'rbacmysql'@'%' IDENTIFIED BY 'rbacmysql'; 
+
+CREATE DATABASE rbacmysql DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+grant all  on rbacmysql.* to 'rbacmysql'@'%' ;
+
+use rbacmysql;
